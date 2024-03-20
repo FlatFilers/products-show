3e2e55e5709aedc79a61109361cc9cd458bb67b6
John Mosesman <johnmosesman@gmail.com>
2024-03-20 16:13:48 -0500

    more

--- lib/get-server-session.ts
+++ lib/get-server-session.ts
@@ -1,6 +1,6 @@
 import { getServerSession as nextGetServerSession } from "next-auth/next";
 import { Session } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/next-auth-options";
 
 export function getServerSession(): Promise<Session | null> {
   return nextGetServerSession(authOptions);
