3d632727974cccb5fdca12094c76670ae3d12d30
John Mosesman <johnmosesman@gmail.com>
2024-03-20 17:18:41 -0500

    style signup

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,9 +1,15 @@
 import SignupForm from "@/app/(auth)/signup/signup-form";
+import SVG from "react-inlinesvg";
 
 export default function Page() {
   return (
-    <div>
-      <p>Signup </p>
+    <div className="space-y-8">
+      <div className="flex flex-col items-center w-full">
+        <SVG src="/images/hcm-logo.svg" />
+        <p className="mt-8 text-white text-center text-xl">
+          Create your account
+        </p>
+      </div>
 
       <SignupForm />
     </div>
