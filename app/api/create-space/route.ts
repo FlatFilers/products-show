d7d6671eba6b5bb71cb2190b449286cd3ecdd5ab
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2024-03-27 10:43:14 -0600

    Use authenticated route

--- app/api/create-space/route.ts
+++ app/api/create-space/route.ts
@@ -1,29 +1,28 @@
-import { getServerSession } from "@/lib/get-server-session";
 import { SpaceService } from "@/lib/services/space";
 import { NextRequest, NextResponse } from "next/server";
 import invariant from "ts-invariant";
+import { authenticatedRoute } from "@/lib/api-helpers";
 
-export const POST = async (req: NextRequest) => {
-  const session = await getServerSession();
+export const POST = async (request: NextRequest, context: { params: any }) => {
+  return authenticatedRoute(request, context, async (request, context) => {
+    let json = await request.json();
+    invariant(json.workflowType, "Requires workflow type");
+    invariant(json.spaceName, "Requires space name");
 
-  invariant(session, "Requires session");
-  invariant(session.user, "Requires user");
+    const userId = context.user.id;
 
-  let json = await req.json();
-  invariant(json.workflowType, "Requires workflow type");
-  invariant(json.spaceName, "Requires space name");
+    let space;
+    try {
+      space = await SpaceService.createSpace({
+        workflowType: json.workflowType,
+        userId,
+        spaceName: json.spaceName,
+      });
+    } catch (e) {
+      console.error(`Error creating space for ${userId}`, e);
+      return new NextResponse("Error creating space", { status: 500 });
+    }
 
-  let space;
-  try {
-    space = await SpaceService.createSpace({
-      workflowType: json.workflowType,
-      userId: session.user.id,
-      spaceName: json.spaceName,
-    });
-  } catch (e) {
-    console.error(`Error creating space for ${session.user.id}`, e);
-    return new NextResponse("Error creating space", { status: 500 });
-  }
-
-  return NextResponse.json({ spaceId: space.id }, { status: 201 });
+    return NextResponse.json({ spaceId: space.id }, { status: 201 });
+  });
 };
