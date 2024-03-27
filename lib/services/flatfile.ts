fb9d9ee1914d2e540bfdc2fa30b9d63e9a38605a
Carl Brugger <carl.brugger@flatfile.io>
2024-03-27 15:26:18 -0500

    Switch to Flatfile SDK

--- lib/services/flatfile.ts
+++ lib/services/flatfile.ts
@@ -1,4 +1,4 @@
-import { FlatfileClient } from "@flatfile/api";
+import api from "@flatfile/api";
 
 export class FlatfileService {
   static createSpace = async ({
@@ -8,41 +8,15 @@ export class FlatfileService {
     userId: string;
     spaceName: string;
   }) => {
-    // const flatfile = this.flatfileClient();
-    // const result = await flatfile.spaces.create({
-    //   name: spaceName,
-    //   environmentId: process.env.FLATFILE_ENVIRONMENT_ID,
-    //   autoConfigure: true,
-    //   namespace: process.env.FLATFILE_NAMESPACE,
-    //   metadata: {
-    //     userId,
-    //   },
-    // });
-
-    const result = await fetch("https://api.x.flatfile.com/v1/spaces", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.FLATFILE_API_KEY}`,
+    const { data } = await api.spaces.create({
+      name: spaceName,
+      environmentId: process.env.FLATFILE_ENVIRONMENT_ID,
+      autoConfigure: true,
+      namespace: process.env.FLATFILE_NAMESPACE,
+      metadata: {
+        userId,
       },
-      body: JSON.stringify({
-        name: spaceName,
-        environmentId: process.env.FLATFILE_ENVIRONMENT_ID,
-        autoConfigure: true,
-        namespace: process.env.FLATFILE_NAMESPACE,
-        metadata: {
-          userId,
-        },
-      }),
     });
-
-    const json = await result.json();
-    return json.data;
+    return data;
   };
-
-  static flatfileClient() {
-    return new FlatfileClient({
-      token: process.env.FLATFILE_API_KEY,
-    });
-  }
 }
