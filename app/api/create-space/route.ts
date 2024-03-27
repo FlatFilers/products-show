013702964d332f2833b2e1af7891e16b98fa1af9
Carl Brugger <cebrugg@gmail.com>
2024-03-27 16:06:18 -0500

    Switch to Flatfile SDK (#8)

--- app/api/create-space/route.ts
+++ app/api/create-space/route.ts
@@ -1,7 +1,7 @@
+import { authenticatedRoute } from "@/lib/api-helpers";
 import { SpaceService } from "@/lib/services/space";
 import { NextRequest, NextResponse } from "next/server";
 import invariant from "ts-invariant";
-import { authenticatedRoute } from "@/lib/api-helpers";
 
 export const POST = async (request: NextRequest, context: { params: any }) => {
   return authenticatedRoute(request, context, async (request, context) => {
@@ -11,18 +11,16 @@ export const POST = async (request: NextRequest, context: { params: any }) => {
 
     const userId = context.user.id;
 
-    let space;
     try {
-      space = await SpaceService.createSpace({
+      const space = await SpaceService.createSpace({
         workflowType: json.workflowType,
         userId,
         spaceName: json.spaceName,
       });
+      return NextResponse.json({ spaceId: space.id }, { status: 201 });
     } catch (e) {
       console.error(`Error creating space for ${userId}`, e);
       return new NextResponse("Error creating space", { status: 500 });
     }
-
-    return NextResponse.json({ spaceId: space.id }, { status: 201 });
   });
 };
