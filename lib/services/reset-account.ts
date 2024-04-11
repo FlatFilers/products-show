faaf534497aa14796c425d2e072c5347effbe27e
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2024-04-11 14:52:55 -0600

    Use transaction

--- lib/services/reset-account.ts
+++ lib/services/reset-account.ts
@@ -3,34 +3,29 @@ import { prismaClient } from "@/lib/prisma-client";
 export class ResetAccountService {
   static resetAccount = async ({ userId }: { userId: string }) => {
     console.log(`Resetting account for user ${userId}`);
+
     // Reset account logic here
-
-    // delete categories
-    prismaClient.category.deleteMany({
-      where: {
-        userId,
-      },
-    });
-
-    // delete suppliers
-    prismaClient.supplier.deleteMany({
-      where: {
-        userId,
-      },
-    });
-
-    // delete products
-    prismaClient.product.deleteMany({
-      where: {
-        userId,
-      },
-    });
-
-    // delete spaces
-    prismaClient.space.deleteMany({
-      where: {
-        userId,
-      },
+    await prismaClient.$transaction(async (prisma) => {
+      prisma.category.deleteMany({
+        where: {
+          userId,
+        },
+      });
+      prisma.supplier.deleteMany({
+        where: {
+          userId,
+        },
+      });
+      prisma.product.deleteMany({
+        where: {
+          userId,
+        },
+      });
+      prisma.space.deleteMany({
+        where: {
+          userId,
+        },
+      });
     });
 
     // TODO: Reseed initial data
