9c5cd7939180bb44b05ee04f11c85abc0df8b024
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2024-04-15 16:08:00 -0600

    Wrong spot

--- lib/dynamic/space-configure.ts
+++ lib/dynamic/space-configure.ts
@@ -1,18 +1,15 @@
 import { configureSpace } from "@flatfile/plugin-space-configure";
 import { blueprint } from "@/lib/dynamic/blueprint";
 import { FlatfileListener } from "@flatfile/listener";
-import { theme } from "@/lib/dynamic/theme";
-import { document } from "@/lib/dynamic/document";
 
 export function spaceConfigure(listener: FlatfileListener) {
   listener.use(
     configureSpace({
       space: {
         metadata: {
-          theme,
+          // add theme here
         },
       },
-      documents: [document],
       workbooks: [
         {
           name: "Field Services Import",
