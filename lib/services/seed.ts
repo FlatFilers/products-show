93e34a38479ff26f0ee6b2db79a27977d8ca28d9
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2024-04-17 13:53:02 -0600

    Use reseed in seed function

--- lib/services/seed.ts
+++ lib/services/seed.ts
@@ -6,10 +6,7 @@ export class SeedService {
   static async seed() {
     const user = await this.upsertUser();
 
-    await this.upsertAttributes(user.id);
-    await this.upsertCategories(user.id);
-    await this.upsertSuppliers(user.id);
-    await this.upsertProducts(user.id);
+    await this.reseed({ userId: user.id });
   }
 
   static async reseed({ userId }: { userId: string }) {
