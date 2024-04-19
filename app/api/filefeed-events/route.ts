a9adf44c8b38477ec165277587675329ceaf5423
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2024-04-19 10:52:08 -0600

    Only v1 routes

--- app/api/filefeed-events/route.ts
+++ app/api/filefeed-events/route.ts
@@ -3,55 +3,6 @@ import { serializeFilefeedEvents } from "@/lib/serializers/filefeed-event";
 import { ActionService } from "@/lib/services/action";
 import { NextRequest, NextResponse } from "next/server";
 
-/**
- * @swagger
- * /api/filefeed-events:
- *   get:
- *     tags: [/api/]
- *     summary: Retrieve all filefeed events for the logged-in user
- *     description: Returns a list of filefeed events associated with the logged-in user.
- *     parameters:
- *       - in: header
- *         name: Authorization
- *         required: true
- *         schema:
- *           type: string
- *         description: Bearer token for authentication.
- *       - in: header
- *         name: x-listener-auth
- *         required: true
- *         schema:
- *           type: string
- *       - in: header
- *         name: x-space-id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Successfully retrieved the filefeed events.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 actions:
- *                   type: array
- *                   items:
- *                     type: object
- *                     properties:
- *                       id:
- *                         type: string
- *                       actionType:
- *                         type: string
- *                       createdAt:
- *                         type: string
- *                       data:
- *                         type: object
- *       401:
- *         description: Unauthorized, if the user is not logged in or token is invalid.
- */
-
 export const GET = async (request: NextRequest, context: { params: any }) => {
   return authenticatedRoute(request, context, async (rq, cxt) => {
     const actions = await ActionService.getFilefeedEvents({
--- app/api/visit-space/[spaceId]/route.ts
+++ app/api/visit-space/[spaceId]/route.ts
@@ -3,46 +3,6 @@ import invariant from "ts-invariant";
 import { authenticatedRoute } from "@/lib/api-helpers";
 import { SpaceService } from "@/lib/services/space";
 
-/**
- * @swagger
- * /api/visit-space/{spaceId}:
- *   get:
- *     tags: [/api/]
- *     summary: Get guest link for space
- *     description: Returns the guest link for the space.
- *     parameters:
- *       - in: header
- *         name: Authorization
- *         required: true
- *         schema:
- *           type: string
- *         description: Bearer token for authentication.
- *       - in: header
- *         name: x-listener-auth
- *         required: true
- *         schema:
- *           type: string
- *       - in: path
- *         name: spaceId
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Successfully retrieved the guest link.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 guestLink:
- *                   type: string
- *       401:
- *         description: Unauthorized, if the user is not logged in or token is invalid.
- *       500:
- *         description: Error getting guest link.
- */
-
 export const GET = async (request: NextRequest, context: { params: any }) => {
   return authenticatedRoute(request, context, async (_req, ctx) => {
     const spaceId = ctx.params.spaceId;
