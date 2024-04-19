b28aed3fccd534c4c138026987443c2223565046
Gaelyn Cooper <57960885+gaelyn@users.noreply.github.com>
2024-04-19 13:18:26 -0600

    Add actions to activity log (#37)
    * Add actions to activity log
    
    * tweak style
    
    ---------
    
    Co-authored-by: John Mosesman <johnmosesman@gmail.com>

---
+++ app/(authenticated)/(content)/activity-log/activity-log.tsx
@@ -0,0 +1,60 @@
+import { ActionService } from "@/lib/services/action";
+import { DateTime } from "luxon";
+import { getServerSession } from "@/lib/get-server-session";
+import invariant from "ts-invariant";
+
+export default async function ActivityLog() {
+  const session = await getServerSession();
+  invariant(session?.user, "User must be logged in");
+
+  const actions = await ActionService.getActions({
+    userId: session.user.id,
+  });
+
+  return (
+    <div className="mt-8 flex flex-col">
+      <div className="overflow-x-auto">
+        <div className="inline-block min-w-full py-2 align-middle">
+          <div className="overflow-hidden shadow md:rounded-lg">
+            <table className="ui-table min-w-full">
+              <thead>
+                <tr>
+                  <th scope="col">Action</th>
+                  <th scope="col">User</th>
+                  <th scope="col">Date</th>
+                  <th scope="col">Result</th>
+                </tr>
+              </thead>
+              <tbody>
+                {actions &&
+                  actions.length > 0 &&
+                  actions.map((a) => {
+                    return (
+                      <tr key={a.id}>
+                        <td>{a.type}</td>
+                        <td className="secondary">{a.user.email}</td>
+                        <td>
+                          {DateTime.fromISO(
+                            a.createdAt.toISOString()
+                          ).toLocaleString()}
+                        </td>
+                        <td>{a.description}</td>
+                      </tr>
+                    );
+                  })}
+
+                {(!actions || actions.length === 0) && (
+                  <tr>
+                    <td colSpan={4} className="secondary">
+                      No actions taken yet
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/(authenticated)/(content)/activity-log/page.tsx
+++ app/(authenticated)/(content)/activity-log/page.tsx
@@ -1,44 +1,23 @@
-"use client";
+import ActivityLog from "@/app/(authenticated)/(content)/activity-log/activity-log";
+import ResetAccountButton from "@/app/(authenticated)/(content)/activity-log/reset-account-button";
 
-import { Button } from "@/components/ui/button";
-import { ArrowPathRoundedSquareIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
-import { useToast } from "@/components/ui/use-toast";
-
-export default function Page() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const { toast } = useToast();
-
-  async function onSubmit() {
-    if (confirm("Are you sure you want to reset your account?")) {
-      setIsSubmitting(true);
-      localStorage.clear();
-      try {
-        await fetch("/api/reset-account", {
-          method: "POST",
-        });
-
-        toast({
-          title: "Account Reset",
-        });
-      } catch (error) {
-        console.error("Error resetting account", error);
-      }
-    }
-
-    setIsSubmitting(false);
-  }
-
+export default async function Page() {
   return (
     <div>
-      <Button
-        disabled={isSubmitting}
-        onClick={onSubmit}
-        className={`rounded-xl text-base focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 sm:w-auto}`}
-      >
-        {isSubmitting ? "Resetting Account..." : "Reset Account"}
-        <ArrowPathRoundedSquareIcon className="w-5 h-5 ml-2" />
-      </Button>
+      <div className="px-4 lg:px-0">
+        <div className="sm:flex sm:items-center">
+          <div className="w-full flex flex-row justify-between mb-8">
+            <div className="sm:flex-auto">
+              <h1 className="text-xl font-semibold">Activity Log</h1>
+              <p className="mt-2 text-sm text-gray-400">
+                Your activity history from Flatfile will show here.
+              </p>
+            </div>
+            <ResetAccountButton />
+          </div>
+        </div>
+      </div>
+      <ActivityLog />
     </div>
   );
 }
